8cbecc28a41a30989c3b47bea26e32286a97dbb1
Seshdev <seshdev.rath@bharatpe.com>
2021-02-02 16:13:49 +0530

    initial commit

---
+++ src/main/java/com/kafkaproducer/example/resources/UserResource.java
@@ -0,0 +1,9 @@
+package com.kafkaproducer.example.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserResource {
+}
